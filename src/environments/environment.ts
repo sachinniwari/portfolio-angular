91a2aa79b9aa0c261f7e160c89edcace62e16966
Sachin Shrivastava <sachinshrivastava955@gmail.com>
2023-04-25 17:34:52 +0530

    img

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  imageURL:"https://sachinniwari.github.io/portfolio-live-angular/"
 };
 
 /*
